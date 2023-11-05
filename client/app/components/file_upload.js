c3dc020e89fa597c66722865c9a3746aa6358411
Purav <puravparabdev@gmail.com>
2023-11-04 17:21:30 -0700

    upload api successfully integrated

--- client/app/components/file_upload.js
+++ client/app/components/file_upload.js
@@ -12,7 +12,6 @@ const FileUpload = () => {
 		
 		if (file) {
 			setDroppedFile(file);
-			console.log(file)
 		}
 	}
 
@@ -20,8 +19,31 @@ const FileUpload = () => {
 		e.preventDefault()
 	}
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
+	const handleSubmit = async () => {
+		console.log(droppedFile)
+		if (droppedFile){
+			const url = process.env.NEXT_PUBLIC_SERVER_URL + "upload/"
+			console.log(url)
+			// Create a FormData object and append the file to it
+			const formData = new FormData();
+			formData.append('file', droppedFile);
+
+			// Replace 'your_api_endpoint' with the actual API endpoint where you want to submit the file
+			const res = await fetch(url, {
+				method: 'POST',
+				body: formData,
+				headers: {
+				},
+			})
+			if (res.ok){
+				const responseData = await res.json();
+				console.log(responseData)
+				console.log('File submitted successfully!')
+			}
+			else{
+				console.error('File submission failed.');
+			}
+		}
 	}
 
 	return (
--- server/upload/views.py
+++ server/upload/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(['POST'])
 def upload_file(request, format=None):
 	file = request.data.get("file")
-	return Response({"res": file}, status=status.HTTP_200_OK)
+	return Response({"text": file}, status=status.HTTP_200_OK)
